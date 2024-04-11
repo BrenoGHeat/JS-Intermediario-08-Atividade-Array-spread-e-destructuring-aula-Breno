d5b5438367d15c78fcbcf28834933315cff1198d
Breno Garcia <BrenoGHeat@gmail.com>
2024-04-11 14:03:07 -0300

    resolvido

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,38 +3,63 @@
   dbUsers.js e dbAdress.js, respecitvamente
 */
 
+import { enderecos } from './dbAddress.js' ;
+import { usuarios } from './dbUsers.js';
+
+
+
 // Faça as importações corretas acima
 console.log(usuarios);
 console.log(enderecos);
 
 // Adicionar endereços aos usuarios pelo id
 function addAddresToUser(listaDeUsuarios, listaDeEnderecos) {
-  return;
+      const usersWithAdress = listaDeUsuarios.map((user) =>{
+        const { id } = user;
+        const { endereco } = listaDeEnderecos.find((currentAdress) =>{
+          return currentAdress.userId === id;
+        })
+        return {
+          ...user,
+          endereco,
+        };
+      });
+
+  return usersWithAdress;
 }
 
 // Descomente os consoles abaixo para testar suas funções
-// console.log(addAddresToUser(usuarios, enderecos));
+console.log(addAddresToUser(usuarios, enderecos));
 
 // Funcao para deletar o password
-function deletePassword() {
-  return;
-}
-// console.log(deletePassword(usuarios[0]));
+function deletePassword({ password , ...rest} ) {
+
+  return rest;
+};
+
+console.log(deletePassword(usuarios[0]));
 
 // Listar os usuarios sem a senha
 function listUsers(usuarios) {
-  return;
-}
-// console.log(listUsers(usuarios));
+
+  return usuarios.map((user) => deletePassword(user));
+
+};
+console.log(listUsers(usuarios));
 
 // Listar somente nome, email e stack
 function listUserStacks(listaDeUsuarios) {
-  return;
+  return listaDeUsuarios.map(({ nome , email , stacks}) => ({
+    nome,
+    email,
+    stacks,
+  }));
 }
-// console.log(listUserStacks(usuarios));
+ console.log(listUserStacks(usuarios));
 
 //Deletar um usuário que tenha python na stack
 function deleteUser(listaDeUsuarios) {
-  return;
-}
-// console.log(deleteUser(usuarios));
+
+  return listaDeUsuarios.filter(({stacks}) => !stacks.includes("Python")).map((user) => deletePassword(user));
+};
+console.log(deleteUser(usuarios));
